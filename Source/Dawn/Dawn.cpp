8ec2089b631f7a20a77ba0268f1a1992fd878ed4
svvj <wjdtmd0105@gmail.com>
2022-02-17 13:40:56 +0900

    Set init project

---
+++ Source/Dawn/Dawn.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Dawn.h"
+#include "Modules/ModuleManager.h"
+
+DEFINE_LOG_CATEGORY(Dawn);
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Dawn, "Dawn" );
---
+++ Source/Dawn/DawnGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Dawn.h"
+#include "GameFramework/GameModeBase.h"
+#include "DawnGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAWN_API ADawnGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
